6a34ff03474ed13b6e135a914718d607c9a0ab34
Keesvds2006 <105289400+Keesvds2006@users.noreply.github.com>
2022-05-10 14:16:43 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,145 +1,52 @@
 let audioContext;
 
-//FM
-let oscillator, tremolo, vibrato, gain, vibratoGain;
-let fmIsPlaying = false;
+function play(frequency, time) {
 
-//ADDITIVE
-let oscillators, amplifiers;
+  const osc = audioContext.createOscillator();
+  const volume = audioContext.createGain();
 
-//PROCESSING
-let audioBuffer;
+  osc.type = 'square';
+  osc.frequency.value = frequency;
 
-function init() {
+  osc.connect(volume);
+  volume.connect(audioContext.destination);
 
-  if(!audioContext) {
-    console.log(' Init! ');
-    audioContext = new AudioContext();
-  }
+  volume.gain.setValueAtTime(0, time);
+  volume.gain.linearRampToValueAtTime(1, time + 1);
+  volume.gain.linearRampToValueAtTime(0, time + 2);
+
+  osc.start();
+}
+
+function sequence () {
+
+  console.log('sequence');
+
+  audioContext = new AudioContext();
+
+  [100,200,80,60,150].forEach((item, index) => {
+    play(item, audioContext.currentTime + (index * 2));
+  });
 
 }
 
-function fm() {
+function saw(frequencies, duration) {
 
-  init();
+  const osc = audioContext.createOscillator();
+  osc.type = 'sawtooth';
+  osc.frequency.setValueCurveAtTime(frequencies, audioContext.currentTime, duration);
 
-  if(fmIsPlaying) {
-    oscillator.stop();
-    tremolo.stop();
-    vibrato.stop();
-    fmIsPlaying = false;
-  } else {
-    // Audio Controls
-   oscillator = audioContext.createOscillator();
-   tremolo = audioContext.createOscillator();
-   vibrato = audioContext.createOscillator();
-   gain = audioContext.createGain();
-   vibratoGain = audioContext.createGain();
-
-   // Set their values
-   oscillator.frequency.value = 200;
-   tremolo.frequency.value = 20;
-   vibrato.frequency.value = 500;
-   vibratoGain.gain.value = 3000;
-   gain.gain.value = 1.0;
-
-   // Connect the Controls
-   tremolo.connect(gain.gain);
-   vibrato.connect(vibratoGain);
-   vibratoGain.connect(oscillator.frequency);
-   oscillator.connect(gain);
-   gain.connect(audioContext.destination);
-
-   // Start the oscillators
-   oscillator.start();
-   tremolo.start();
-   vibrato.start();
-
-   // Program Changes
-   oscillator.frequency.linearRampToValueAtTime(5000, audioContext.currentTime + 12);
-   tremolo.frequency.exponentialRampToValueAtTime(50, audioContext.currentTime + 12);
-
-   //Manage state
-   fmIsPlaying = true;
-  }
+  osc.connect(audioContext.destination);
+  osc.start();
 
 }
 
-function additive() {
-
-  init();
-
-  oscillators = [];
-  amplifiers = [];
-
-  let times = 200;
-
-  for (var i = 0; i < times; i++) {
-
-    //Create unit generators
-    oscillators[i] = audioContext.createOscillator();
-    amplifiers[i] = audioContext.createGain();
-
-    //Set their values
-    oscillators[i].frequency.value = 100 * (i + 1 * Math.random()*0.4);
-    amplifiers[i].gain.value = 1.0 / times;
-
-    //Connect them
-    oscillators[i].connect(amplifiers[i]);
-    amplifiers[i].connect(audioContext.destination);
-
-    //Play them
-    oscillators[i].start();
-
-    //Stop them
-    oscillators[i].stop(audioContext.currentTime + (5 * Math.random() + 1))
-
-  }
-
+function curve() {
+  audioContext = new AudioContext();
+  let list = [100,200,300].map(function(item) { return item * Math.random() });
+  saw(list, 5 * Math.random());
 }
 
-function processing() {
+document.getElementById('buttonSequence').addEventListener("click", sequence);
 
-  init();
-
-  if(!audioBuffer) {
-    let request = new XMLHttpRequest();
-    request.open('get', 'http://bjarnig.s3.eu-central-1.amazonaws.com/sounds/snd.mp3', true);
-    request.responseType = "arraybuffer";
-
-    request.onload = function() {
-      audioContext.decodeAudioData(request.response, function(buffer) {
-        audioBuffer = buffer;
-        console.log('Sample is loaded')
-      })
-    };
-
-    request.send();
-
-  } else {
-
-    let sample = audioContext.createBufferSource();
-    sample.buffer = audioBuffer;
-    sample.playbackRate.value = 10 * (Math.random() + 0.0001);
-
-    let delay = audioContext.createDelay();
-    delay.delayTime.value = Math.random()*0.01;
-
-    let feedback = audioContext.createGain();
-    feedback.gain.value = 0.9;
-
-    delay.connect(feedback);
-    feedback.connect(delay);
-
-    sample.connect(delay);
-    sample.connect(audioContext.destination);
-    delay.connect(audioContext.destination);
-
-    sample.start(audioContext.currentTime);
-
-  }
-}
-
-document.getElementById('fmbutton').addEventListener("click", fm);
-document.getElementById('addbutton').addEventListener("click", additive);
-document.getElementById('processbutton').addEventListener("click", processing);
+document.getElementById('buttonCurve').addEventListener("click", curve);
